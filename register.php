1082b40633b85474e697789e82d6799ee47000c0
Yash Kumar Arora <arorayash1998@gmail.com>
2019-06-10 16:32:16 +0530

    Initial Commit

---
+++ register.php
@@ -0,0 +1,180 @@
+<?php
+	require_once 'connection.php';
+    session_start();
+    if(isset($_SESSION["uid"])){
+        $id=$_SESSION["uid"];
+        $sql="select * from admin where id=$id LIMIT 1";
+        $run_query=mysqli_query($con,$sql);
+        while($row=mysqli_fetch_array($run_query)){
+            $f=$row["first"];
+   	        $s=$row["second"];
+        }
+    }
+    else
+ 	    header('Location:index.php');
+
+    date_default_timezone_set("Asia/Kolkata");
+
+ ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/rstyle.css">
+	<script type="text/javascript" src="webcamjs/webcam.min.js"></script>
+
+</head>
+<body>
+
+<ul style="position:relative; top:0px;">
+  <li><a href="#" title="logout">Guard : </a></li>
+  <li><a href="action.php" title="visitor info" target="_blank"><?php echo $f." ".$s; ?></a></li>
+  <li><a>Date : <?php echo date("y-m-d"); ?></a></li>
+    <li style="margin-left: 65%;"><a href="destroy.php" title="logout">Logout</a></li>
+
+    </ul>
+
+
+
+
+<form action=""  method="POST" enctype="multipart/form-data">
+  <center><div class="container">
+          <img width="50%" height="50%" src="images/gl.png">
+
+    <h3>VISITOR PASS ENTRY</h3>
+
+    <hr>
+
+    <table>
+<tbody>
+<tr>
+<td style="width: 10%;">Student Name: </td>
+<td style="width: 70%;"><input type="text" placeholder="Full name of Student" name="name" required></td>
+</tr>
+<tr>
+<td style="width: 10%;">Father's Name: </td>
+<td style="width: 70%;"><input type="text" placeholder="Fathers name" name="fname" required></td>
+</tr>
+<tr>
+<td style="width: 30%;">Mobile No: </td>
+<td style="width: 70%;"><input type="text" placeholder="Enter 10 Digit Mobile Number" name="num" pattern="[6789][0-9]{9}" required></td>
+</tr>
+<tr>
+<td style="width: 10%;">City: </td>
+<td style="width: 70%;"><input type="text" placeholder="Enter City" name="city" required></td>
+</tr>
+<tr>
+<td style="width: 10%;">Address: </td>
+<td style="width: 70%;"><input type="text" placeholder="Enter Address" name="address" required></td>
+</tr>
+<tr>
+<td style="width: 10%;">Purpose: </td>
+<td style="width: 70%;"><input type="text" placeholder="Purpose of visit" name="purpose" required></td>
+</tr>
+<tr>
+<td style="width: 10%;">Image: </td>
+<td style="width: 70%;"><input type="file" name="upfile" accept="images/*" required></td>
+</tr>
+<tr>
+<td style="width: 10%;">Gates: </td>
+<td style="width: 70%;"><input type="radio" name="gate" value="Gate 1" required checked> Gate 1 <input type="radio" name="gate" value="Gate 2" required>Gate 2</td>
+</tr>
+
+
+</tbody>
+</table>
+
+    <?php
+       if(!empty($message)){echo $message;} ?>
+
+
+    <br>
+
+
+    <div class="clearfix">
+      <button type="RESET" class="cancelbtn">Reset</button>
+        <a href='generate.php?f=$file_name' target=_blank><button type="submit" class="signupbtn" name="submit">Submit</button></a>
+    </div>
+  </div>
+  <br>
+  <br>
+</form>
+</center>
+<div class="cam">
+   <div id="my_camera"></div><br>
+	<input style="margin-left:16%;" type=button value="Take Snapshot" onClick="take_snapshot()">
+
+    <div id="results" ></div>
+
+    <!-- Configure a few settings and attach camera -->
+	<script type="text/javascript">
+		Webcam.set({
+			width: 320,
+			height: 240,
+			image_format: 'jpeg',
+			jpeg_quality: 90
+		});
+		Webcam.attach( '#my_camera' );
+	</script>
+	<!-- A button for taking snaps -->
+
+	<!-- Code to handle taking the snapshot and displaying it locally -->
+	<script type="text/javascript">
+
+		function take_snapshot() {
+
+			// take snapshot and get image data
+			Webcam.snap( function(data_uri) {
+				// display results in page
+				document.getElementById('results').innerHTML =
+					'<img src="'+data_uri+'"/>';
+			} );
+		}
+	</script>
+</div>
+
+</body>
+</html>
+
+<?php
+    if(isset($_POST["submit"])){
+        $name=$_POST["name"];
+        $fname=$_POST["fname"];
+        $mobile=$_POST["num"];
+        $city=$_POST["city"];
+        $address=$_POST["address"];
+        $date=date("y-m-d");
+        $time=date("h:i:s",time());
+        $fn=$_FILES['upfile'] ['name'];
+        $purpose=$_POST["purpose"];
+        $gate=$_POST["gate"];
+
+        $filebasename=basename($_FILES['upfile'] ['name']);
+        $ext=strtolower(substr($filebasename,strrpos($filebasename,'.')+1));
+
+        if(($ext=="jpg" || $ext=="jpeg" || $ext=="JPG") && ( $_FILES["upfile"]["type"]=="image/jpg" || $_FILES["upfile"]["type"]=="image/jpeg")){
+             $desired_dir="photo/";
+             $file_name=$_FILES['upfile'] ['name'];
+
+             if(file_exists($desired_dir . $file_name)){
+                echo $file_name." already exist.";
+             }
+             else{
+                 $sql="INSERT INTO `visitor`(number,name,address,city,gid,file,purpose,gate,fname) VALUES                      ('$mobile','$name','$address','$city','$id','$file_name','$purpose','$gate','$fname')";
+                 $result=mysqli_query($con,$sql);
+                 if($result){
+                     move_uploaded_file($_FILES["upfile"]["tmp_name"],$desired_dir.$file_name);
+                     echo "Data is SUCESSFULLY UPLOAD.";
+                     echo "<a href='generate.php?f=$file_name' target=_blank>GENERATE PASS</A>";
+                 }
+                 else
+                     echo " error";
+            }
+        }
+        else{
+            echo "INSERTION OF INFORMATION IS FAILED!!";
+        }
+    }
+
+?>
---
+++ visitorsystem.sql
@@ -0,0 +1,81 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: May 09, 2018 at 02:25 PM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 5.6.35
+
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `visitorsystem`
+--
+CREATE DATABASE IF NOT EXISTS visitorsystem;
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` varchar(11) NOT NULL,
+  `first` varchar(10) NOT NULL,
+  `second` varchar(10) NOT NULL,
+  `user` varchar(20) NOT NULL,
+  `password` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `first`, `second`, `user`, `password`) VALUES
+('1000', 'Yash', 'yash', 'yash', 'admin@123'),
+('102', 'Arora', 'arora', 'arora', 'admin@2');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `visitor`
+--
+
+CREATE TABLE `visitor` (
+  `name` varchar(100) DEFAULT NULL,
+  `number` varchar(11) DEFAULT NULL,
+  `city` varchar(20) DEFAULT NULL,
+  `address` varchar(200) DEFAULT NULL,
+  `date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `file` varchar(50) DEFAULT NULL,
+  `gid` varchar(11) DEFAULT NULL,
+  `purpose` varchar(500) DEFAULT NULL,
+  `gate` varchar(10) DEFAULT NULL,
+  `fname` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user` (`user`);
+
+ALTER TABLE `visitor`
+ADD otp INTEGER DEFAULT 0;
+
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
